1db3093f9cb6ab32ce4f5ccf716ae2b8cc33871f
Bruno Ayabe <brunoayabe@edu.unifil.br>
2025-05-20 20:21:18 -0300

    contador

--- src/Arvore.java
+++ src/Arvore.java
@@ -1,4 +1,10 @@
 public class Arvore {
 
     No raiz;
+    
+//Implementar um método para contar os nós da árvore
+    public int contarNos(No node) {
+        if (node == null) return 0;
+        return 1 + contarNos(node.esquerda) + contarNos(node.direita);
+    }
 }
